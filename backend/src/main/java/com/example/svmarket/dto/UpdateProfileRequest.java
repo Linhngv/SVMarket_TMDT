e2dd6fb1d2854b4005873e3bfbcf558e226d12c7
tugiaman22130162 <22130162@st.hcmuaf.edu.vn>
2026-04-12 22:18:38 +0700

    Xử lý phần cập nhật thông tin người dùng và lưu vào DB, uploads hình avatar lên Server

---
+++ backend/src/main/java/com/example/svmarket/dto/UpdateProfileRequest.java
@@ -0,0 +1,11 @@
+package com.example.svmarket.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateProfileRequest {
+    private String fullName;
+    private String university;
+    private String province;
+    private String addressDetail;
+}
