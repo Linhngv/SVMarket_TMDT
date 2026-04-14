39f1331b1874ae27cde4fffcf301cd379103d45f
tugiaman22130162 <22130162@st.hcmuaf.edu.vn>
2026-04-14 15:20:14 +0700

    Tách giao diện trang chủ ra khỏi App.tsx

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
+import Footer from "../components/Footer";
+
+type Props = {
+    isLoggedIn: boolean;
+    avatarUrl: string;
+    userName: string;
+};
+
+function Home({ isLoggedIn, avatarUrl, userName }: Props) {
+    return (
+        <>
+            <Header
+                isLoggedIn={isLoggedIn}
+                avatarUrl={avatarUrl}
+                userName={userName}
+            />
+
+            <Banner />
+
+            <div className="container-fluid px-4 mt-3">
+                <Categories />
+                <Products title="Tất cả bài đăng" />
+                <Products title="Đề xuất sản phẩm" />
+            </div>
+
+            <Footer />
+        </>
+    );
+}
+
+export default Home;
